966015e409bb82062fcbdcae4c10cd2f24d3e613
Marcos Salto <marcosasalto@gmail.com>
2024-08-12 22:20:35 -0300

    Create packages, constants and strings files

---
+++ app/src/main/java/com/marcossalto/pets/core/Constants.kt
@@ -0,0 +1,9 @@
+package com.marcossalto.pets.core
+
+class Constants {
+    companion object {
+        const val DATABASE_NAME = "pets_db"
+        const val PET_TABLE = "pet_table"
+        const val PET_ID = "pet_id"
+    }
+}
